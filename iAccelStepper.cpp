85a8ae52829054197ba2d26352b2cd41cebb6e43
Szymon Bieganski <S.Bieganski@chello.nl>
2014-08-27 09:41:52 +0200

    driverlib HWREG quick write alternative - not tested... yet

--- iAccelStepper.cpp
+++ iAccelStepper.cpp
@@ -1,7 +1,17 @@
 #include "iAccelStepper.h"
 #include "driverlib/timer.h"
+#include "inc/hw_gpio.h"
 
-static iAccelStepper* me[3];
+#define portOutputRegister(x) (regtype)portBASERegister(x)
+typedef volatile uint32_t regtype;
+typedef uint8_t regsize;
+
+static iAccelStepper* me[2];
+static unsigned int ulPort_step[2];
+static unsigned int ucPin_step[2];
+static unsigned int ulPort_dir[2];
+static unsigned int ucPin_dir[2];
+static boolean direction[2];
 static unsigned int all_instances;
 static unsigned long ulPeriod;
 
@@ -17,16 +27,19 @@ void iAccelStepper::ISR(void) {
 
   // prepare for the next period
   computeNewSpeed();
-  digitalWrite(_pin[1], _direction);
+  if(direction[id] != _direction) {
+    direction[id] = _direction;
+    HWREG(ulPort_dir + (GPIO_O_DATA + (ucPin_dir[id]))) = _direction;
+  }
 
   // switch off timer at the falling edge
   if((_stepInterval == 0) || (abs(distanceToGo()) < 1)) {
     TimerDisable(g_ulTIMERBase[id], TIMER_A);
     running = false;
   } else {
-    digitalWrite(_pin[0], true);
+    HWREG(ulPort_step + (GPIO_O_DATA + (ucPin_step[id]))) = true;
     delayMicroseconds(ulPeriod);
-    digitalWrite(_pin[0], false);
+    HWREG(ulPort_step + (GPIO_O_DATA + (ucPin_step[id]))) = false;
 
     TimerLoadSet(g_ulTIMERBase[id], TIMER_A, _stepInterval - ulPeriod);
     TimerEnable(g_ulTIMERBase[id], TIMER_A);
@@ -66,6 +79,10 @@ void iAccelStepper::begin(uint8_t pin1, uint8_t pin2, uint8_t pin3)
     me[id] = this;
     running = false;
     ++all_instances;
+    ulPort_step[id] = portOutputRegister(digitalPinToPort(pin1));
+    ucPin_step[id] = digitalPinToBitMask(pin1);
+    ulPort_dir[id] = portOutputRegister(digitalPinToPort(pin2));
+    ucPin_dir[id] = digitalPinToBitMask(pin2);
   }
 }
 
@@ -83,7 +100,10 @@ void iAccelStepper::moveTo(long absolute)
     // enable driver
 //    enableOutputs();
     computeNewSpeed();
-    digitalWrite(_pin[1], _direction);
+    if(direction[id] != _direction) {
+      direction[id] = _direction;
+      HWREG(ulPort_dir + (GPIO_O_DATA + (ucPin_dir[id]))) = _direction;
+    }
 
     if(_direction == DIRECTION_CW)
       // Clockwise
@@ -92,9 +112,9 @@ void iAccelStepper::moveTo(long absolute)
       // Anticlockwise
       --_currentPos;
 
-    digitalWrite(_pin[0], true);
+    HWREG(ulPort_step + (GPIO_O_DATA + (ucPin_step[id]))) = true;
     delayMicroseconds(ulPeriod);
-    digitalWrite(_pin[0], false);
+    HWREG(ulPort_step + (GPIO_O_DATA + (ucPin_step[id]))) = false;
 
     TimerLoadSet(g_ulTIMERBase[id], TIMER_A, _stepInterval - ulPeriod);
     TimerEnable(g_ulTIMERBase[id], TIMER_A);
